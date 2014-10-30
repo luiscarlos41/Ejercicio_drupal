776e9855d4d09c719274c0f3085286d5e46ab1ed
Carlos <oalo113118@upemor.edu.mx>
2014-10-30 17:55:18 -0600

    lol3

--- sites/all/themes/Carlos_theme/templates/page.tpl.php
+++ sites/all/themes/Carlos_theme/templates/page.tpl.php
@@ -140,7 +140,12 @@
         <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
       <?php endif; ?>
       <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
-      <a id="main-content"></a>
+      
+      <?php if (!empty($action_links)): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <div class="main-content-wrapper">
+        <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if (!empty($title)): ?>
         <h1 class="page-header"><?php print $title; ?></h1>
@@ -153,10 +158,9 @@
       <?php if (!empty($page['help'])): ?>
         <?php print render($page['help']); ?>
       <?php endif; ?>
-      <?php if (!empty($action_links)): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
+       <?php print render($page['content']); ?>
+      </div>
+      
     </section>
 
     <?php if (!empty($page['sidebar_second'])): ?>
